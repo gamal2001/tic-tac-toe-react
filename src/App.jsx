da6dcd45f55c17f0332b8add312bb0ce6b2ca80b
gamal2001 <kmhamak@gmail.com>
2023-09-08 02:31:25 +0300

    Add Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,74 @@
 import "./App.css";
+import { useState } from "react";
 const EMPTY = "EMPTY";
 const CIRCLE = "CIRCLE";
 const CROSS = "CROSS";
 function TicTacToe() {
-  const player = CIRCLE;
-  const position = [
-    EMPTY,
-    EMPTY,
-    EMPTY,
-    CIRCLE,
-    EMPTY,
-    CROSS,
-    EMPTY,
-    EMPTY,
-    EMPTY,
-  ];
+  const [state, setState] = useState({
+    player: CIRCLE,
+    positions: Array(9).fill(EMPTY),
+  });
+
+  function takeTurn(position) {
+    const positions = [...state.positions];
+    positions[position] = state.player;
+    setState({
+      player: state.player === CIRCLE ? CROSS : CIRCLE,
+      positions,
+    });
+  }
   return (
     <div className="grid">
-      <Square position={0} value={position[0]} />
-      <Square position={1} value={position[1]} />
-      <Square position={2} value={position[2]} />
-      <Square position={3} value={position[3]} />
-      <Square position={4} value={position[4]} />
-      <Square position={5} value={position[5]} />
-      <Square position={6} value={position[6]} />
-      <Square position={7} value={position[7]} />
-      <Square position={8} value={position[8]} />
+      <Square position={0} value={state.positions[0]} takeTurn={takeTurn} />
+      <Square position={1} value={state.positions[1]} takeTurn={takeTurn} />
+      <Square position={2} value={state.positions[2]} takeTurn={takeTurn} />
+      <Square position={3} value={state.positions[3]} takeTurn={takeTurn} />
+      <Square position={4} value={state.positions[4]} takeTurn={takeTurn} />
+      <Square position={5} value={state.positions[5]} takeTurn={takeTurn} />
+      <Square position={6} value={state.positions[6]} takeTurn={takeTurn} />
+      <Square position={7} value={state.positions[7]} takeTurn={takeTurn} />
+      <Square position={8} value={state.positions[8]} takeTurn={takeTurn} />
     </div>
   );
 }
 
-function Square({position,value}) {
+function Square({ position, value, takeTurn }) {
+  function handleClick() {
+    if (value === EMPTY) {
+      takeTurn(position);
+    }
+  }
   return (
-  <div className='square'>
-      {value === CIRCLE && <Circle/>}
-      {value === CROSS && <Cross/>}
-      
-  </div>
-);
+    <div className="square" onClick={handleClick}>
+      {value === CIRCLE && <Circle />}
+      {value === CROSS && <Cross />}
+    </div>
+  );
 }
 
 function Circle() {
   return (
-    <>
-      <div>Circle</div>
-    </>
-  )
+    <svg width="100" height="100" viewBox="-50 -50 100 100" className="circle">
+      <circle cx="0" cy="0" r="40" />
+    </svg>
+  );
 }
 
 function Cross() {
   return (
-    <>
-      <div>Cross</div>
-    </>
-  )
+    <svg width="100" height="100" viewBox="-50 -50 100 100" className="cross">
+      <line x1="-40" y1="-40" x2="40" y2="40" />
+      <line x1="-40" y1="40" x2="40" y2="-40" />
+    </svg>
+  );
 }
 
 function Result() {
   return (
-  <div>
-    <h1>Result</h1>
-  </div>
-);
+    <div>
+      <h1>Result</h1>
+    </div>
+  );
 }
 
 export default TicTacToe;
