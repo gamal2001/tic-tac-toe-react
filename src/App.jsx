2a72edaca4d42b886108cfe3258f27b778ce70ad
gamal2001 <kmhamak@gmail.com>
2023-09-08 03:33:11 +0300

    detect winner

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,32 @@ import { useState } from "react";
 const EMPTY = "EMPTY";
 const CIRCLE = "CIRCLE";
 const CROSS = "CROSS";
+function detectWinner(p) {
+  if (p[0] == CIRCLE && p[1] == CIRCLE && p[2] == CIRCLE) return CIRCLE;
+  if (p[3] == CIRCLE && p[4] == CIRCLE && p[5] == CIRCLE) return CIRCLE;
+  if (p[6] == CIRCLE && p[7] == CIRCLE && p[8] == CIRCLE) return CIRCLE;
+
+  if (p[0] == CIRCLE && p[3] == CIRCLE && p[6] == CIRCLE) return CIRCLE;
+  if (p[1] == CIRCLE && p[4] == CIRCLE && p[7] == CIRCLE) return CIRCLE;
+  if (p[2] == CIRCLE && p[5] == CIRCLE && p[8] == CIRCLE) return CIRCLE;
+
+  if (p[0] == CIRCLE && p[4] == CIRCLE && p[8] == CIRCLE) return CIRCLE;
+  if (p[2] == CIRCLE && p[4] == CIRCLE && p[6] == CIRCLE) return CIRCLE;
+
+  if (p[0] == CROSS && p[1] == CROSS && p[2] == CROSS) return CROSS;
+  if (p[3] == CROSS && p[4] == CROSS && p[5] == CROSS) return CROSS;
+  if (p[6] == CROSS && p[7] == CROSS && p[8] == CROSS) return CROSS;
+
+  if (p[0] == CROSS && p[3] == CROSS && p[6] == CROSS) return CROSS;
+  if (p[1] == CROSS && p[4] == CROSS && p[7] == CROSS) return CROSS;
+  if (p[2] == CROSS && p[5] == CROSS && p[8] == CROSS) return CROSS;
+
+  if (p[0] == CROSS && p[4] == CROSS && p[8] == CROSS) return CROSS;
+  if (p[2] == CROSS && p[4] == CROSS && p[6] == CROSS) return CROSS;
+
+  if (p.every((position) => position != EMPTY)) return "It is a tie";
+}
+
 function TicTacToe() {
   const [state, setState] = useState({
     player: CIRCLE,
@@ -17,17 +43,33 @@ function TicTacToe() {
       positions,
     });
   }
+
+  function reset() {
+    setState({
+      player: CIRCLE,
+      positions: [
+        EMPTY, EMPTY, EMPTY,
+        EMPTY, EMPTY, EMPTY,
+        EMPTY, EMPTY, EMPTY
+      ]
+    })
+  }
+
+  const winner = detectWinner(state.positions);
   return (
-    <div className="grid">
-      <Square position={0} value={state.positions[0]} takeTurn={takeTurn} />
-      <Square position={1} value={state.positions[1]} takeTurn={takeTurn} />
-      <Square position={2} value={state.positions[2]} takeTurn={takeTurn} />
-      <Square position={3} value={state.positions[3]} takeTurn={takeTurn} />
-      <Square position={4} value={state.positions[4]} takeTurn={takeTurn} />
-      <Square position={5} value={state.positions[5]} takeTurn={takeTurn} />
-      <Square position={6} value={state.positions[6]} takeTurn={takeTurn} />
-      <Square position={7} value={state.positions[7]} takeTurn={takeTurn} />
-      <Square position={8} value={state.positions[8]} takeTurn={takeTurn} />
+    <div>
+      <div className="grid">
+        <Square position={0} value={state.positions[0]} takeTurn={takeTurn} />
+        <Square position={1} value={state.positions[1]} takeTurn={takeTurn} />
+        <Square position={2} value={state.positions[2]} takeTurn={takeTurn} />
+        <Square position={3} value={state.positions[3]} takeTurn={takeTurn} />
+        <Square position={4} value={state.positions[4]} takeTurn={takeTurn} />
+        <Square position={5} value={state.positions[5]} takeTurn={takeTurn} />
+        <Square position={6} value={state.positions[6]} takeTurn={takeTurn} />
+        <Square position={7} value={state.positions[7]} takeTurn={takeTurn} />
+        <Square position={8} value={state.positions[8]} takeTurn={takeTurn} />
+      </div>
+      {winner && <Result winner={winner} reset={reset} />}
     </div>
   );
 }
@@ -63,10 +105,13 @@ function Cross() {
   );
 }
 
-function Result() {
+function Result({ winner, reset }) {
   return (
-    <div>
-      <h1>Result</h1>
+    <div className="result">
+      {winner == CIRCLE && "Circle won the game"}
+      {winner == CROSS && "Cross won the game"}
+      {winner == "It is a tie" && "It is a tie"}
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
